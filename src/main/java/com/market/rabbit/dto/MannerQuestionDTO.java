43ef2c3af6aec193747ab4b8b026aa11b238ed4e
CHOHWIYOON\Hwiyoon <gold5784_2@hanmail.net>
2021-04-23 14:20:20 +0900

    댓글, 자주묻는질문, 키워드, 매너질문, 멤버 dto

--- src/main/java/com/market/rabbit/dto/MannerQuestionDTO.java
+++ src/main/java/com/market/rabbit/dto/MannerQuestionDTO.java
@@ -4,5 +4,44 @@ import org.apache.ibatis.type.Alias;
 
 @Alias("mannerQuestion")
 public class MannerQuestionDTO {
+	
+	/*
+	MANNER_IDX	NUMBER(10,0)
+	MANNER_CONTENT	NVARCHAR2(200 CHAR)
+	TARGET	NVARCHAR2(20 CHAR)
+	TRADE_TYPE	NVARCHAR2(20 CHAR)
+	 */
+	
+	private int manner_idx;
+	private String manner_content;
+	private String target;
+	private String trade_type;
+	
+	public int getManner_idx() {
+		return manner_idx;
+	}
+	public void setManner_idx(int manner_idx) {
+		this.manner_idx = manner_idx;
+	}
+	public String getManner_content() {
+		return manner_content;
+	}
+	public void setManner_content(String manner_content) {
+		this.manner_content = manner_content;
+	}
+	public String getTarget() {
+		return target;
+	}
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	public String getTrade_type() {
+		return trade_type;
+	}
+	public void setTrade_type(String trade_type) {
+		this.trade_type = trade_type;
+	}
+	
+	
 
 }
