7b8e0953dadba867b3c6cbacdd2a4adb37df8874
yunso66 <yunso6695@gmail.com>
2021-05-02 14:28:43 +0900

    로그인/회원가입

--- src/main/java/com/market/rabbit/member/controller/HomeController.java
+++ src/main/java/com/market/rabbit/member/controller/HomeController.java
@@ -1,24 +1,114 @@
 package com.market.rabbit.member.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.market.rabbit.dto.MemberDTO;
+import com.market.rabbit.member.service.MemberService;
 
 
 @Controller
 public class HomeController {
-	
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home(Model model) {
 
+	String plain ="";
+	String hash ="";
+	
+	@Autowired MemberService service;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping(value = "/member/memberLogin",  method = RequestMethod.GET)
+	public String memberLogin( Model model) {
+
+		return "member/memberLogin";
+	}
+	@RequestMapping(value = "/sale/mainPage",  method = RequestMethod.GET)
+	public String mainPage( Model model) {
 		
-		return "home";
+		return "sale/mainPage";
 	}
 	
+	
+	@RequestMapping(value = "/member/overlay",  method = RequestMethod.POST)
+	public @ResponseBody HashMap<String, Object> overlay( Model model, @RequestParam String id) {
+		logger.info("고객이 입력한 아이디 : "+id);
+		 return service.overlay(id);
+	}
+	@RequestMapping(value = "/member/emoverlay",  method = RequestMethod.POST)
+	public @ResponseBody HashMap<String, Object> emoverlay( Model model, @RequestParam String email) {
+		logger.info("고객이 입력한 이메일 : "+email);
+		return service.emoverlay(email);
+	}
+	
+	@RequestMapping(value = "/member/join", method = RequestMethod.POST)
+	public String join(Model model, @ModelAttribute MemberDTO dto
+			,@RequestParam String pw) {
+		logger.info("회원 정보 입력 값 = "+dto.getAddress());
+		logger.info("회원가입 페이지");
+	
+		plain = pw;
+		logger.info("평문"+plain);
+		BCryptPasswordEncoder en = new BCryptPasswordEncoder();
+		hash = en.encode(plain); //암호화`
+		logger.info("암호문"+hash);
+		dto.setPw(hash);
+		logger.info("회원 정보 입력 값 = "+dto.getPw());
+
+		String msg = "회원 가입에 실패했습니다. 잠시 후 다시 시도해주세요";
+		String page = "redirect:/";
+		if(service.join(dto) > 0) {
+			msg = "회원 가입 성공";
+			page = "member/memberLogin";
+		}
+		
+		model.addAttribute("msg", msg);
+		
+		return page;
+}
+	
+	@RequestMapping(value = "/member/login", method = RequestMethod.POST)
+	public ModelAndView login(Model model,@RequestParam HashMap<String, Object> params,
+			 RedirectAttributes rAttr,HttpSession session, @RequestParam String LoginPw, MemberDTO dto) {
+		logger.info("login : "+params);	
+		//비밀번호 암호
+				BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+				//입력된 평문, 암호화된 내용
+				 boolean success = encoder.matches(LoginPw, dto.getPw());//비교
+				 logger.info("입력전 패스워드 :"+LoginPw);	
+				 logger.info("입력후 패스워드 :"+success);	
+				 
+		return service.login(params, rAttr,session);
+	}
+	
+	@RequestMapping(value = "/member/memberRegist", method = RequestMethod.GET)
+	public String MemberRegist( Model model) {
+		
+		return "member/memberRegist";
+	}
+	
+	@RequestMapping(value = "/member/memberId", method = RequestMethod.GET)
+	public String MemberId( Model model) {
+		
+		return "member/memberId";
+	}
+	
+	@RequestMapping(value = "/member/memberPw", method = RequestMethod.GET)
+	public String MemberPw( Model model) {
+		
+		return "member/memberPw";
+	}
 }
--- src/main/java/com/market/rabbit/member/service/MemberService.java
+++ src/main/java/com/market/rabbit/member/service/MemberService.java
@@ -1,8 +1,89 @@
 package com.market.rabbit.member.service;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.market.rabbit.dto.MemberDTO;
+import com.market.rabbit.member.dao.MemberDAO;
+
 
 @Service
 public class MemberService {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired MemberDAO dao;
+	ModelAndView mav = new ModelAndView();
+	String page = "";
+	String msg = "";
+	
+	public int join(MemberDTO dto) {
+		
+		return dao.join(dto);
+	}
 
+
+	public HashMap<String, Object> overlay(String id) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		int use = dao.overlay(id);
+		map.put("use",use);
+		return map;
+	}
+
+	public HashMap<String, Object> emoverlay(String email) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		int use2 = dao.emoverlay(email);
+		map.put("use2",use2);
+		return map;
+	}
+
+	public ModelAndView login(HashMap<String, Object> params, RedirectAttributes rAttr, HttpSession session) {
+		logger.info("로그인 서비스 요청");
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("login_id", params.get("LoginId"));
+		map.put("pw", params.get("LoginPw"));
+		logger.info("map"+map);
+		
+		//일반인 모드로 오면 
+		if(params.get("mode").equals("member")) {
+			logger.info("일반인 모드");
+			
+			//id를 비교해서
+			if(dao.login(map) > 0 ) {
+				page="sale/mainPage";
+				
+				session.setAttribute("loginId", params.get("LoginId"));
+				logger.info("로그인 성공");
+			}else {
+				page="redirect:/member/memberLogin";
+				msg = "아이디와 패스워드를 확인해 주세요.";
+			}
+		}else {
+			if(dao.admin(map) > 0) {
+				logger.info("관리자 모드");
+				page ="sale/mainPage";
+				
+				session.setAttribute("adminId", params.get("LoginId"));
+			}else {
+				page="redirect:/member/memberLogin";
+				msg = "관리자 아이디와 패스워드를 다시 확인해주세요";
+			}
+		}
+		
+		rAttr.addFlashAttribute("msg2", msg);		
+		mav.setViewName(page);	
+		return mav;
+	}
+
+
+	
 }
